174afde652e1d64492e8f65a9198d6e0f970351e
Fredrik Ridderfalk <ridderfalk@gmail.com>
2024-03-25 20:42:23 +0100

    add @for syntax

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,6 +7,54 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  recipes: any[] = [];
 
+  tortillaSoup = {
+    title: 'Tortilla Soup ⭐️',
+    description: 'This is a delicious recipe for tortilla soup.',
+    recipeImage: '../assets/images/tortilla-soup.jpg',
+  };
+  poké = {
+    title: 'Poké Bowl 🥗',
+    description: 'This is a delicious recipe for poké.',
+    recipeImage: '../assets/images/poke.jpg',
+  };
+  arancini = {
+    title: 'Arancini 🍚',
+    description: 'This is a delicious recipe for arancini.',
+    recipeImage: '../assets/images/arancini.jpeg',
+  };
+  burger = {
+    title: 'Burger 🍔',
+    description: 'This is a delicious recipe for a burger.',
+    recipeImage: '../assets/images/burger.jpg',
+  };
+  orangeTofu = {
+    title: 'Orange Tofu 🍊',
+    description: 'This is a delicious recipe for orange tofu.',
+    recipeImage: '../assets/images/orange-tofu.jpg',
+  };
+  ramen = {
+    title: 'Ramen 🍜',
+    description: 'This is a delicious recipe for ramen.',
+    recipeImage: '../assets/images/ramen.png',
+  };
+
+  constructor() {
+    console.log('HomeComponent constructor');
+  }
+
+  ngOnInit() {
+    console.log('HomeComponent initialized');
+    this.recipes = [
+      this.tortillaSoup,
+      this.poké,
+      this.arancini,
+      this.burger,
+      this.orangeTofu,
+      this.ramen,
+    ];
+    console.log(this.recipes);
+  }
 }
--- src/app/pages/recipe/recipe.component.ts
+++ src/app/pages/recipe/recipe.component.ts
@@ -5,12 +5,11 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <h1 class="text-green-500">{{title}}</h1>
-    <img src="{{recipeImage}}" alt="{{title}}">
-    <p>{{description}}</p>
+    <h1 class="text-green-500">{{ title }}</h1>
+    <img src="{{ recipeImage }}" alt="{{ title }}" />
+    <p>{{ description }}</p>
   `,
 })
-
 export class RecipeComponent {
   title = 'Tortilla Soup ⭐️';
   description = 'This is a delicious recipe for tortilla soup.';
