a87f7cdebbec93a04d83434d468f113923071e2e
Fredrik Ridderfalk <ridderfalk@gmail.com>
2024-03-31 19:12:17 +0200

    add updateServings()

--- src/app/pages/recipe/recipe.component.ts
+++ src/app/pages/recipe/recipe.component.ts
@@ -10,4 +10,16 @@ export class RecipeComponent {
   title = 'Tortilla Soup ⭐️';
   description = 'This is a delicious recipe for tortilla soup.';
   recipeImage = '../assets/images/tortilla-soup.jpg';
+
+  isDropdownOpen = false;
+  servings = 2;
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  updateServings(value: number): void {
+    this.servings = value;
+    this.toggleDropdown();
+  }
 }
