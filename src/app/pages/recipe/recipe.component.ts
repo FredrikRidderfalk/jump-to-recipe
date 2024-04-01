a69e50f85cea09b46bcbdb7e8a92092e8bfe4ca0
Fredrik Ridderfalk <ridderfalk@gmail.com>
2024-04-01 16:42:46 +0200

    add recipe object

--- src/app/pages/recipe/recipe.component.ts
+++ src/app/pages/recipe/recipe.component.ts
@@ -7,6 +7,168 @@ import { Component } from '@angular/core';
   templateUrl: './recipe.component.html',
 })
 export class RecipeComponent {
+  recipe = {
+    name: 'Tortilla Soup',
+    recipeImage: '../assets/images/tortilla-soup.jpg',
+    star: true,
+    description:
+      'A hearty and flavorful soup that originated in Mexico. A one-pot dish ready in just 15 minutesand we can add just about anything in it. The soup is usually made with pieces of fried corn tortillas, which add a crunchy texture to the soup. It also features canned beans, and we add all the aquafaba that comes with it. The soup is often garnished with toppings such as diced avocado, cilantro, red onion, and sour cream. It is a satisfying and filling meal that is perfect for a cold day.',
+    tags: {
+      quick: true,
+      easy: true,
+      temperature: {
+        value: '',
+        unit: '',
+      },
+      lesswash: true,
+      cheap: true,
+    },
+    ingredients: [
+      {
+        name: 'Water',
+        amount: 600,
+        unit: 'ml',
+      },
+      {
+        name: 'Puréed tomatoes',
+        amount: 200,
+        unit: 'ml',
+      },
+      {
+        name: 'Vegetable bouillon',
+        amount: 2,
+        unit: 'cubes',
+      },
+      {
+        name: 'Beans in aquafaba',
+        amount: 400,
+        unit: 'ml',
+      },
+      {
+        name: 'Bell pepper',
+        amount: 1,
+        unit: '',
+      },
+      {
+        name: 'BBQ sauce',
+        amount: 50,
+        unit: 'ml',
+      },
+      {
+        name: 'MSG',
+        amount: 1,
+        unit: 'tbsp',
+      },
+      {
+        name: 'Sugar',
+        amount: 1,
+        unit: 'tbsp',
+      },
+      {
+        name: 'Chili flakes',
+        amount: 1,
+        unit: 'tbsp',
+      },
+      {
+        name: 'Avocado',
+        amount: 1,
+        unit: '',
+      },
+      {
+        name: 'Red onion',
+        amount: 0.5,
+        unit: '',
+      },
+      {
+        name: 'Cilantro',
+        amount: 50,
+        unit: 'ml',
+      },
+      {
+        name: 'Ketchup',
+        amount: 50,
+        unit: 'ml',
+      },
+      {
+        name: 'Light soy sauce',
+        amount: 50,
+        unit: 'ml',
+      },
+      {
+        name: 'Tortilla chips',
+        amount: null,
+        unit: '',
+      },
+      {
+        name: 'Oregano',
+        amount: 1,
+        unit: 'tbsp',
+      },
+      {
+        name: 'Corn',
+        amount: 100,
+        unit: 'g',
+      },
+      {
+        name: 'Jackfruit',
+        amount: 100,
+        unit: 'g',
+      },
+      {
+        name: 'Liquid smoke',
+        amount: 2,
+        unit: 'tsp',
+      },
+      {
+        name: 'Lime',
+        amount: 1,
+        unit: '',
+      },
+    ],
+    flex: [
+      {
+        ingredient: 'Vegetables',
+        substitutes:
+          'The bell pepper, corn, jackfruit, cilantro, and toppings can be substituted with anything. You can add more vegetables, but also add fewer vegetables than what we listed.',
+      },
+      {
+        ingredient: 'Beans',
+        substitutes: 'Kidney beans, great white beans, black beans, anything goes!',
+      },
+      {
+        ingredient: 'MSG',
+        substitutes: 'Not necessary, but a flavor enhancer.',
+      },
+      {
+        ingredient: 'Chili flakes',
+        substitutes:
+          'Or fresh chopped up chili if we have it. We often add a hot sauce instead, or a chili paste like Tobanjjan or tabasco.',
+      },
+      {
+        ingredient: 'Tortilla chips',
+        substitutes:
+          "This is added for crunch, so any chips fit. For example, we also love adding salt & vinegar chips, onion chips, BBQ chips, or if we don't have chips we may even go with roasted onion.",
+      },
+      {
+        ingredient: 'Lime',
+        substitutes: "Use lime, lemon, or yuzu, or we can use any vinegar in half the amount if we don't have citrus.",
+      },
+      {
+        ingredient: 'Puréed tomatoes',
+        substitutes: 'We can use canned crushed tomatoes instead, or even chop up fresh tomatoes ourselves.',
+      },
+    ],
+    instructions: [
+      '1. we add all the ingredients except the toppings and lime to a pot and bring to a boil. The toppings are the avocado, red onion, and tortilla chips. Remember to add the aquafaba with the beans into the pot.',
+      '2. While bringing the pot to a boil, we chop up the toppings.',
+      '3. We boil on medium-high heat for about 5 min. We taste the soup during this time, and add more of any ingredient we wish.',
+      '4. When it has finished boiling we add the lime, and we also bring some extra lime to the table for us to squeeze in as we desire.',
+      '5. We serve the toppings in bowls on the table for each person to freely take.',
+    ],
+    notes:
+      'This soup is slightly different each time we make it thanks to its versitility. Sometimes we also add plant-based melty cheese to it or plant-based sour cream with garlic.',
+  };
+
   title = 'Tortilla Soup';
   description = 'This is a delicious recipe for tortilla soup.';
   recipeImage = '../assets/images/tortilla-soup.jpg';
