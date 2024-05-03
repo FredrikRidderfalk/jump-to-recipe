8d58751cd4e2c8bc6e444e49765a40aac0815d9e
Fredrik Ridderfalk <ridderfalk@gmail.com>
2024-05-03 13:26:52 +0200

    unsubscribe using takeUntil operator

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Recipes } from '../../../assets/recipes/recipes';
 import { TagsComponent } from '../../components/tags.component';
 import { UpperCasePipe } from '@angular/common';
 import { ApiService } from '../../services/api.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +12,11 @@ import { ApiService } from '../../services/api.service';
   imports: [RouterLink, UpperCasePipe, TagsComponent],
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   recipes: any[] = [];
   recipeViews: any = [];
+
+  private destroy$ = new Subject<void>();
 
   constructor(private apiService: ApiService) {}
 
@@ -21,14 +24,22 @@ export class HomeComponent implements OnInit {
     this.recipes = Recipes;
     console.log(this.recipes);
 
-    this.apiService.getRecipeViews().subscribe({
-      next: (data) => {
-        this.recipeViews = data;
-        console.log('recipe views 👀', this.recipeViews);
-      },
-      error: (error) => {
-        console.error('There was an error!', error);
-      },
-    });
+    this.apiService
+      .getRecipeViews()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (data) => {
+          this.recipeViews = data;
+          console.log('recipe views 👀', this.recipeViews);
+        },
+        error: (error) => {
+          console.error('There was an error!', error);
+        },
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
