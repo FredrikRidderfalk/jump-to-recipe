b17e812958e89bdbeb2e3ad3096d395f6266619d
Fredrik Ridderfalk <ridderfalk@gmail.com>
2025-04-20 17:18:15 +0200

    update recipe

--- src/assets/recipes/overnightOats.ts
+++ src/assets/recipes/overnightOats.ts
@@ -6,7 +6,7 @@ export const overnightOats = {
   introText:
     'These easy and nutritious overnight oats are perfect for busy mornings. Packed with fiber, protein, and healthy fats, they make a balanced and delicious breakfast that you can prepare ahead of time.',
   description:
-    'Overnight oats are a simple make-ahead breakfast option that’s both healthy and customizable. Rolled oats are soaked in plant-based milk, combined with chia seeds, flax seeds, a hint of cinnamon, and a touch of sweetness from syrup. Top them with your favorite nuts, seeds, or dried fruits for extra texture and flavor.',
+    'Overnight oats are a simple make-ahead breakfast option that’s both healthy and customizable. Oats are soaked in plant-based milk, combined with chia seeds, flax seeds, a hint of cinnamon, and a touch of sweetness from syrup. Top them with your favorite nuts, seeds, or dried fruits for extra texture and flavor.',
   tags: {
     quick: true,
     easy: true,
@@ -134,12 +134,11 @@ export const overnightOats = {
     },
   ],
   instructions: [
-    'In a bowl or jar, combine the oats, milk, syrup, chia seeds, sesame seeds, nutritional yeast, flax seeds, cinnamon, and salt.',
+    'In a bowl or jar, combine the oats, milk, syrup, chia seeds, sesame seeds, nutritional yeast, optional flax seeds, cinnamon, and salt.',
     'Mix everything together until well combined.',
-    'Cover and store in the refrigerator for at least one hour, or preferably overnight, to allow the oats to soak and thicken.',
+    'Let sit for at least 20 minutes, or even overnight in the refrigerator, to allow the chia to soak and thicken.',
     'When ready to eat, top with your favorite toppings like pumpkin seeds, walnuts, sunflower seeds, or dried cranberries.',
-    'Serve chilled and enjoy a nutrient-packed start to your day.',
   ],
   notes:
-    'Feel free to adjust the sweetness or add more spices like nutmeg or cardamom for extra flavor. You can also experiment with different plant-based milks or add a spoonful of peanut butter or fruit preserves for added richness.',
+    'Feel free to adjust the sweetness or add more spices like nutmeg, cocoa powder, or cardamom for extra flavor. You can also experiment with different plant-based milks or add a spoonful of peanut butter or fruit preserves for added richness.',
 };
