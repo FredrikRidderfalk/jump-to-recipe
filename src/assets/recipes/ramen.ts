00a78c1d3bde1e7c0d3d1704ea58ef82a7dbfcba
Fredrik Ridderfalk <ridderfalk@gmail.com>
2025-07-17 13:50:27 -0600

    update recipe

---
+++ src/assets/recipes/ramen.ts
@@ -0,0 +1,30 @@
+export const ramen = {
+  name: 'Ramen',
+  param: 'ramen',
+  recipeImage: 'assets/images/ramen.png',
+  star: true,
+  introText:
+    'A comforting and satisfying noodle soup that is perfect for any day. This ramen recipe is quick, easy, and customizable with your favorite ingredients.',
+  description:
+    "Ramen is a Japanese noodle soup that has taken the world by storm. With its rich broth, tender noodles, and a variety of toppings, it's no wonder that ramen is a favorite comfort food for many.",
+  tags: {
+    quick: true,
+    easy: true,
+    temperature: null,
+    clean: true,
+    cheap: true,
+  },
+  ingredients: [
+    {
+      name: 'Noodles',
+      amount: 200,
+      unit: 'g',
+      flex: '',
+      optional: false,
+      topping: false,
+      done: false,
+    },
+  ],
+  instructions: ['Bring a pot or pan of water to a boil, then salt the water to your liking and add the noodles.'],
+  notes: 'MSG is amazing in this dish.',
+};
