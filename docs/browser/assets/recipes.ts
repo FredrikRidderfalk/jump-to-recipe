dc08ecaa8d47b7275370ec6d578e2e826d4091ae
Fredrik Ridderfalk <ridderfalk@gmail.com>
2026-03-31 17:21:12 +0200

    build app

---
+++ docs/browser/assets/recipes.ts
@@ -0,0 +1,218 @@
+import { brownies } from './recipes/brownies';
+import { streuselBananaMuffins } from './recipes/streusel-banana-muffins';
+import { danDanNoodles } from './recipes/dan-dan-noodles';
+import { tapenade } from './recipes/tapenade';
+import { pesto } from './recipes/pesto';
+import { chiliOilNoodles } from './recipes/chili-oil-noodles';
+import { stickyBananaSpongePudding } from './recipes/sticky-banana-sponge-pudding';
+import { lentilStew } from './recipes/lentil-stew';
+import { coffeeTresLeches } from './recipes/coffee-tres-leches';
+import { southwestDressing } from './recipes/southwest-dressing';
+import { lemonTahiniDressing } from './recipes/lemon-tahini-dressing';
+import { chiliLimeDressing } from './recipes/chili-lime-dressing';
+import { sesameGingerDressing } from './recipes/sesame-ginger-dressing';
+import { tomatoPasta } from './recipes/tomato-pasta';
+import { thaiBasilSauce } from './recipes/thai-basil-sauce';
+import { rootVegetableStew } from './recipes/root-vegetable-stew';
+import { quinoaSalad } from './recipes/quinoa-salad';
+import { crispyChiliOil } from './recipes/crispy-chili-oil';
+import { kladdkaka } from './recipes/kladdkaka';
+import { balsamicDressing } from './recipes/balsamic-vinaigrette';
+import { thaiGreenCurry } from './recipes/thai-green-curry';
+import { chiaOats } from './recipes/chia-oats';
+import { arancini } from './recipes/arancini';
+import { burger } from './recipes/burger';
+import { garlicNoodles } from './recipes/garlic-noodles';
+import { taquitos } from './recipes/taquitos';
+import { bulgurSalad } from './recipes/bulgur-salad';
+import { bulgurPilaf } from './recipes/bulgur-pilaf';
+import { padKraPao } from './recipes/pad-kra-pao';
+import { hummus } from './recipes/hummus';
+import { salsa } from './recipes/salsa';
+import { gamjaJorim } from './recipes/gamja-jorim';
+import { pancakes } from './recipes/pancakes';
+import { dryCaramel } from './recipes/dry-caramel';
+import { iceCream } from './recipes/ice-cream';
+import { cremeBrulee } from './recipes/creme-brulee';
+import { waffles } from './recipes/waffles';
+import { kakiage } from './recipes/kakiage';
+import { peanutButterCornflakeBars } from './recipes/peanut-butter-cornflake-bars';
+import { mushroomPolenta } from './recipes/mushroom-polenta';
+import { mutabbal } from './recipes/mutabbal';
+import { cinnamonRolls } from './recipes/cinnamon-rolls';
+import { breakBread } from './recipes/break-bread';
+import { saffronMarzipanSwirls } from './recipes/saffron-marzipan-swirls';
+import { okonomiyaki } from './recipes/okonomiyaki';
+import { tteokbokki } from './recipes/tteokbokki';
+import { scallionPancakes } from './recipes/scallion-pancakes';
+import { chocolateChipCookies } from './recipes/chocolate-chip-cookies';
+import { earlGreyHempCookies } from './recipes/earl-grey-hemp-cookies';
+import { yakiniku } from './recipes/vegan-yakiniku';
+import { sikilPak } from './recipes/sikil-pak';
+import { ponche } from './recipes/ponche';
+import { crispyRiceSalad } from './recipes/crispy-rice-salad';
+import { mushroomBourguignon } from './recipes/mushroomBourguignon';
+import { spongeCake } from './recipes/sponge-cake';
+import { mushroomPasta } from './recipes/mushroom-pasta';
+import { orangeTofu } from './recipes/orange-tofu';
+import { bananaBread } from './recipes/banana-bread';
+import { mirzaGhasemi } from './recipes/mirza-ghasemi';
+import { scallionSesameWaffles } from './recipes/scallion-sesame-waffles';
+import { mapoTofu } from './recipes/mapo-tofu';
+import { poke } from './recipes/poke';
+import { karēRaisu } from './recipes/karē-raisu';
+import { americanCinnamonRolls } from './recipes/american-cinnamon-rolls';
+import { shakshuka } from './recipes/shakshuka';
+import { chickpeaCurry } from './recipes/chickpea-curry';
+import { makhani } from './recipes/makhani';
+import { gochujangPasta } from './recipes/gochujang-pasta';
+import { rhubarbPie } from './recipes/rhubarb-pie';
+import { gochujangNoodles } from './recipes/gochujang-noodles';
+import { lemonPepperGarlicChickpeas } from './recipes/lemon-pepper-garlic-chickpeas';
+import { leonChimichurri } from './recipes/leon-chimichurri';
+import { tortillaSoup } from './recipes/tortilla-soup';
+import { oatMuffins } from './recipes/oat-muffins';
+import { bananaMuffins } from './recipes/banana-muffins';
+import { bananaOatMuffins } from './recipes/banana-oat-muffins';
+import { blueberryYuzuMuffins } from './recipes/blueberry-yuzu-muffins';
+import { mangoSerranoMuffins } from './recipes/mango-serrano-muffins';
+import { muffins } from './recipes/muffins';
+import { basilStrawberryMuffins } from './recipes/basil-strawberry-muffins';
+import { cupcakes } from './recipes/cupcakes';
+import { pestoPasta } from './recipes/pesto-pasta';
+import { pestoMelt } from './recipes/pesto-melt';
+import { tagine } from './recipes/tagine';
+import { swedishBalls } from './recipes/swedish-balls';
+import { ramen } from './recipes/ramen';
+import { serranoSalsa } from './recipes/serrano-salsa';
+import { guacamole } from './recipes/guacamole';
+import { chilaquiles } from './recipes/chilaquiles';
+import { pozole } from './recipes/pozole';
+import { garlicPasta } from './recipes/garlic-pasta';
+import { aguaFrescaDeTamarindo } from './recipes/agua-fresca-de-tamarindo';
+import { chocolateMuffins } from './recipes/chocolate-muffins';
+import { baklavaMuffins } from './recipes/baklava-muffins';
+import { carrotMuffins } from './recipes/carrot-muffins';
+import { lemongrassSoup } from './recipes/lemongrass-soup';
+import { peanutNoodles } from './recipes/peanut-noodles';
+import { stroganoff } from './recipes/stroganoff';
+import { picoDeGallo } from './recipes/pico-de-gallo';
+import { chocolateTea } from './recipes/chocolate-tea';
+import { frijolesRefritos } from './recipes/frijoles-refritos';
+import { chiliConCarne } from './recipes/chili-con-carne';
+import { hokkienMee } from './recipes/hokkien-mee';
+import { silkenTofuStew } from './recipes/silken-tofu-stew';
+import { mangoChutnyCurry } from './recipes/mango-chutney-curry';
+
+export const Recipes = [
+  chiliOilNoodles,
+  peanutNoodles,
+  serranoSalsa,
+  guacamole,
+  picoDeGallo,
+  stickyBananaSpongePudding,
+  pesto,
+  tapenade,
+  taquitos,
+  lentilStew,
+  chilaquiles,
+  pozole,
+  aguaFrescaDeTamarindo,
+  coffeeTresLeches,
+  southwestDressing,
+  lemonTahiniDressing,
+  chiliLimeDressing,
+  dryCaramel,
+  sesameGingerDressing,
+  quinoaSalad,
+  crispyChiliOil,
+  kladdkaka,
+  balsamicDressing,
+  thaiGreenCurry,
+  rootVegetableStew,
+  chiaOats,
+  arancini,
+  burger,
+  garlicNoodles,
+  bulgurSalad,
+  bulgurPilaf,
+  padKraPao,
+  hummus,
+  salsa,
+  gamjaJorim,
+  pancakes,
+  cremeBrulee,
+  waffles,
+  kakiage,
+  peanutButterCornflakeBars,
+  iceCream,
+  mushroomPolenta,
+  mutabbal,
+  cinnamonRolls,
+  breakBread,
+  saffronMarzipanSwirls,
+  okonomiyaki,
+  tteokbokki,
+  scallionPancakes,
+  chocolateChipCookies,
+  earlGreyHempCookies,
+  crispyRiceSalad,
+  mushroomBourguignon,
+  mirzaGhasemi,
+  poke,
+  karēRaisu,
+  makhani,
+  americanCinnamonRolls,
+  shakshuka,
+  chickpeaCurry,
+  ramen,
+  swedishBalls,
+  thaiBasilSauce,
+  tomatoPasta,
+  spongeCake,
+  mushroomPasta,
+  orangeTofu,
+  bananaBread,
+  scallionSesameWaffles,
+  mapoTofu,
+  gochujangPasta,
+  rhubarbPie,
+  gochujangNoodles,
+  lemonPepperGarlicChickpeas,
+  leonChimichurri,
+  tortillaSoup,
+  muffins,
+  cupcakes,
+  pestoPasta,
+  pestoMelt,
+  tagine,
+  garlicPasta,
+  lemongrassSoup,
+  stroganoff,
+  brownies,
+  danDanNoodles,
+  yakiniku,
+  sikilPak,
+  ponche,
+  chocolateTea,
+  frijolesRefritos,
+  chiliConCarne,
+  hokkienMee,
+  silkenTofuStew,
+  mangoChutnyCurry,
+];
+
+// All recipes including alternative versions or base recipes
+export const AllRecipes = [
+  ...Recipes,
+  bananaMuffins,
+  bananaOatMuffins,
+  blueberryYuzuMuffins,
+  mangoSerranoMuffins,
+  oatMuffins,
+  basilStrawberryMuffins,
+  chocolateMuffins,
+  streuselBananaMuffins,
+  baklavaMuffins,
+  carrotMuffins,
+];
